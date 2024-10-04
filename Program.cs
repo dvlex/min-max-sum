57bc9ee0fd7accf5c114b854baf94138b96efbc7
Alex Castillo <alex@lexdrel.com>
2024-10-04 18:24:12 +0000

    final solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+List<long> arr = [1,5,2,4,3];
+
+Result.miniMaxSum(arr);
+
+
+class Result
+{
+    public static void miniMaxSum(List<long> arr)
+    {
+        arr.Sort();
+        long minSum = arr.Take(4).Sum();
+        long maxSum = arr.TakeLast(4).Sum();
+        Console.WriteLine("{0} {1}", minSum, maxSum);
+    }
+
+}
